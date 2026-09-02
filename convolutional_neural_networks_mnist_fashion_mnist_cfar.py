aa1022007e65cc3dbd40f411374202b07ced4fe0
Letícia Zorzi Rama <ramaleticia@univali.br>
2026-09-02 17:02:54 -0300

    Add transfer-learning and fine-tuning sections

--- convolutional_neural_networks_mnist_fashion_mnist_cfar.py
+++ convolutional_neural_networks_mnist_fashion_mnist_cfar.py
@@ -1,33 +1,36 @@
 # TO DOs
-# gpu
-# transferir para jupyter notebook
+# Transferir para jupyter notebook
+# GPU
+# Plotagens com métricas de treino e validação 
 
 # Importações
 
-import numpy as np
+import sys
+
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.model_selection import train_test_split
 
-import sys
 print('Python version:', sys.version.split(' ')[0])
 
 import tensorflow as tf
-import cudnn
+import tensorflow_datasets as tfds
+from tensorflow import data as tf_data
+
 print('TensorFlow version:', tf.__version__)
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 import keras
-from keras import layers
-from keras import ops
-from keras import datasets
+from keras import callbacks, layers
+from keras.applications.resnet_v2 import ResNet50V2, preprocess_input
 
-# Utilidades
+# Funções de plotagens
 
 def plots1(history):
   plt.figure(figsize=(14,4))
   plt.subplot(1,2,1)
   plt.plot(history.history['loss'], '.-', label='Train loss')
-  if 'val_loss' in history.history.keys():
+  if 'val_loss' in history.history:
     plt.plot(history.history['val_loss'], '.-', label='Val loss')
   plt.xlabel('Epochs')
   plt.legend()
@@ -35,7 +38,7 @@ def plots1(history):
   plt.subplot(1,2,2)
   plt.plot(history.history['accuracy'], '.-', label='Train accuracy')
   plt.xlabel('Epochs')
-  if 'val_accuracy' in history.history.keys():
+  if 'val_accuracy' in history.history:
     plt.plot(history.history['val_accuracy'], '.-', label='Val accuracy')
   plt.legend()
   plt.grid()
@@ -112,7 +115,7 @@ x_val = x_train[-5000:] # 5000 amostras finais
 y_val = y_train[-5000:] 
 x_train = x_train[:-5000] # conjunto menos as 5000 amostras finais
 y_train = y_train[:-5000]
-x_train.dtype  
+print(x_train.dtype)
 input_shape = x_train.shape[-2:]
 
 # Redimensionar as dimensões de x externamente (só para salvar aqui, porque está sendo feito internamente nos modelos)
@@ -127,7 +130,7 @@ input_shape = x_train.shape[-2:]
 # Converter vetores y para matrizes binárias
 y_train = keras.utils.to_categorical(y_train, len(classes))
 y_test = keras.utils.to_categorical(y_test, len(classes))
-y_train.shape
+print(y_train.shape)
 
 # 2. Usando o Keras, construa uma rede neural com pelo menos uma camada convolucional (tf.keras.layers.Conv2D) e 
 # confirme que não há nenhum erro de definição. Organize seu código em uma função de criação do modelo, conforme o 
@@ -270,7 +273,7 @@ print(f"Model {model} | Training {training} - Test accuracy: {round(score[1]*100
 
 # Visualização dos resultados
 mod21_plots1 = plots1(history)
-mod22_plots2 = plots2(history, model, training, batch_size, epochs)
+mod21_plots2 = plots2(history, model, training, batch_size, epochs)
 
 # TREINAMENTO 2
 training = 2
@@ -325,7 +328,7 @@ for cls, count in zip(classes, counts):
 # Converter vetores y para matrizes binárias
 y_train = keras.utils.to_categorical(y_train, len(classes))
 y_test = keras.utils.to_categorical(y_test, len(classes))
-y_train.shape
+print(y_train.shape)
 
 # Visualização do dataset
 class_names = [
@@ -400,7 +403,7 @@ print(f"Model {model} | Training {training} - Test accuracy: {round(score[1]*100
 
 # Visualização dos resultados
 mod31_plots1 = plots1(history)
-mod32_plots2 = plots2(history, model, training, batch_size, epochs)
+mod31_plots2 = plots2(history, model, training, batch_size, epochs)
 
 # TREINAMENTO 2
 training = 2
@@ -434,8 +437,8 @@ mod32_plots2 = plots2(history, model, training, batch_size, epochs)
 # ==================
 
 # Carregar dataset
-from tensorflow.keras.datasets import cifar10
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+cifar = tf.keras.datasets.cifar10
+(x_train, y_train), (x_test, y_test) = cifar.load_data()
 print('x_train.dtype:', x_train.dtype)
 print(x_train.shape, y_train.shape)
 print(x_test.shape, y_test.shape)
@@ -469,7 +472,7 @@ for i in range(5):
     img = x_train[y_train == c][i]
     plt.imshow(img)
     if i == 0:
-      plt.title('y = {}'.format(c))
+      plt.title(f'y = {c}')
     plt.axis('off')    
 
 # 6. Inicialmente, apenas converta a mesma arquitetura utilizada no MNIST para o formato das imagens do CIFAR-10 e treine o modelo. 
@@ -522,22 +525,22 @@ print(f"Model {model} | Training {training} - Test accuracy: {round(score[1]*100
 
 # Visualização dos resultados
 mod41_plots1 = plots1(history)
-mod42_plots2 = plots2(history, model, training, batch_size, epochs)
+mod41_plots2 = plots2(history, model, training, batch_size, epochs)
 
 # 7. Por que você acha que isso acontece? Explique.
 # Porque o CIFAR-10 é um dataset mais complexo que o MNIST e o Fashion-MNIST em 
 # termos de dimensões das imagens (32 x 32 x 3) e a arquitetura já se torna simples para
 # ser utilizada no CIFAR-10. 
 # As imagens do CIFAR-10, inclusive, além de coloridas, são mais heterogêneas, apresentando cenários mais
-# próximos do mundo real, diferentemente dos datasets MNIST e Fashion-MNIST cujas imagens são mais homogêneas
-# e abstratas.
+# próximos do mundo real. Soma-se ainda o fato das imagens serem de baixa resolução. 
+# As imagens dos datasets MNIST e Fashion-MNIST, diferentemente, além de monocromáticas, 
+# são mais homogêneas e abstratas.
 
 # Perturbação dos dados
-from tensorflow.keras.layers import RandomFlip, RandomTranslation
 data_augmentation = keras.Sequential(
     [
-        RandomTranslation(height_factor=0.1, width_factor=0.1),
-        RandomFlip(mode='horizontal'),
+        layers.RandomTranslation(height_factor=0.1, width_factor=0.1),
+        layers.RandomFlip(mode='horizontal'),
     ],
     name='data_augmentation',
 )
@@ -594,11 +597,13 @@ model5 = make_model()
 model5.summary()
 
 # Hiperparâmetros
+batch_size = 128
+epochs = 45
 opt = keras.optimizers.Adam(learning_rate=0.01)
 model5.compile(loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 
 # Treinamento
-model5.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
+model5.fit(x_train, y_train, batch_size=batch_size, epochs=epochs)
 
 # Resultados
 history = model5.history
@@ -608,4 +613,195 @@ print(f"Model {model} | Training {training} - Test accuracy: {round(score[1]*100
 
 # Visualização dos resultados
 mod51_plots1 = plots1(history)
+mod51_plots2 = plots2(history, model, training, batch_size, epochs)
+
+# TREINAMENTO 2
+training = 2
+
+# Construir a arquitetura
+model5 = make_model()
+model5.summary()
+
+# Hiperparâmetros
+batch_size = 128
+epochs = 90 
+
+opt = keras.optimizers.Adam(learning_rate=0.01)
+model5.compile(loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+# Adicionar callback de decaimento
+reduce_lr = callbacks.ReduceLROnPlateau(
+    monitor="accuracy",
+    factor=0.1,
+    patience=10,
+    min_lr=0.0,
+)
+
+# Treinamento
+model5.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, callbacks=[reduce_lr])
+
+# Resultados
+history = model5.history
+score = model5.evaluate(x_test, y_test, verbose=0)
+print(f"Model {model} | Training {training} - Test loss: {score[0]}")
+print(f"Model {model} | Training {training} - Test accuracy: {round(score[1]*100, 2)} %") 
+
+# Visualização dos resultados
+mod52_plots1 = plots1(history)
 mod52_plots2 = plots2(history, model, training, batch_size, epochs)
+
+# =================================
+#  TRANSFER LEARNING e FINE-TUNING 
+# =================================
+
+# ======================
+#  CATS VS. DOG dataset
+# ======================
+
+# Carregar dataset
+train_ds, validation_ds, test_ds = tfds.load(
+    "cats_vs_dogs",
+    # Reserve 10% for validation and 10% for test
+    split=["train[:40%]", "train[40%:50%]", "train[50%:60%]"],
+    as_supervised=True,  # Include labels
+)
+
+print(f"Number of training samples: {train_ds.cardinality()}")
+print(f"Number of validation samples: {validation_ds.cardinality()}")
+print(f"Number of test samples: {test_ds.cardinality()}")
+
+# Dimensões de uma imagem
+for i, (image, label) in enumerate(train_ds.take(1)):
+    print(image[0].shape)
+
+# Visualizar amostras do dataset
+plt.figure(figsize=(10, 10))
+for i, (image, label) in enumerate(train_ds.take(9)):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(image)
+    plt.title(int(label))
+    plt.axis("off")
+
+# Redimensionar os dados
+resize_fn = keras.layers.Resizing(150, 150)
+
+train_ds = train_ds.map(lambda x, y: (resize_fn(x), y))
+validation_ds = validation_ds.map(lambda x, y: (resize_fn(x), y))
+test_ds = test_ds.map(lambda x, y: (resize_fn(x), y))
+
+# Atributos do dataset - dimensões de uma amostra após redimensionar
+for i, (image, label) in enumerate(train_ds.take(1)):
+    print(image[0].shape)
+
+# Perturbação dos dados
+augmentation_layers = [
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.1),
+]
+
+def data_augmentation(x):
+    for layer in augmentation_layers:
+        x = layer(x)
+    return x
+
+train_ds = train_ds.map(lambda x, y: (data_augmentation(x), y))
+
+# Batch e prefetching ?
+batch_size = 64
+
+train_ds = train_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
+validation_ds = validation_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
+test_ds = test_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
+
+# Visualizar amostra do dataset de treino após perturbação
+for images, labels in train_ds.take(1):
+    plt.figure(figsize=(10, 10))
+    first_image = images[0]
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        augmented_image = data_augmentation(np.expand_dims(first_image, 0))
+        plt.imshow(np.array(augmented_image[0]).astype("int32"))
+        plt.title(int(labels[0]))
+        plt.axis("off")
+
+# ==========
+#  MODELO 6
+# ==========
+model = 6
+
+# Treinamento 1
+training = 1
+
+# Carregar modelo Xception pré-treinado
+base_model = keras.applications.Xception(
+    weights="imagenet",  # Carregar pesos do modelo treinado com o ImageNet
+    input_shape=(150, 150, 3),
+    include_top=False, # Não incluir o classificador no topo da arquitetura
+) 
+
+# Congelar
+base_model.trainable = False
+
+# Criar um novo modelo no topo da arquitetura
+inputs = keras.Input(shape=(150, 150, 3))
+
+# Xception requer que o input seja de [-1., +1] e offset de 1
+scale_layer = keras.layers.Rescaling(scale=1 / 127.5, offset=-1)
+x = scale_layer(inputs)
+
+# O modelo base contém camadas de batch normalization que devem ser mantidas 
+# no modo de inferência quando a arquitetura for descongelada para o fine-tuning
+# Verificar se o modelo está rodando no modo de inferência
+x = base_model(x, training=False)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dropout(0.2)(x)  
+outputs = keras.layers.Dense(1)(x)
+model6 = keras.Model(inputs, outputs)
+
+# Visualizar a arquitetura com o modelo base congelado (Trainable = N)
+model6.summary(show_trainable=True)
+
+# Hiperparâmetros
+epochs = 2
+model6.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
+
+# Treinar apenas o topo do modelo
+model6.fit(train_ds, epochs=epochs, validation_data=validation_ds, shuffle=False)
+
+# Resultados
+history = model6.history
+score = model6.evaluate(test_ds, verbose=0)
+print(f"Transfer-learning \n Model {model} | Training {training} - Test loss: {score[0]}")
+print(f"Transfer-learning \n Model {model} | Training {training} - Test accuracy: {round(score[1]*100, 2)} %") 
+
+# FINE-TUNING
+# Descongelar o modelo
+# Irá continuar rodando no modo inferência
+
+# Descongelar
+base_model.trainable = True
+
+# Visualizar a arquitetura com o modelo base treinável (Trainable = Y)
+model6.summary(show_trainable=True)
+
+# Hiperparâmetros
+epochs = 1
+model6.compile(
+    optimizer=keras.optimizers.Adam(1e-5),  
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
+
+# Treinar apenas o topo do modelo
+model6.fit(train_ds, epochs=epochs, validation_data=validation_ds, shuffle=False)
+
+# Resultados
+history = model6.history
+score = model6.evaluate(test_ds, verbose=0)
+print(f"Fine-tuning after Transfer-learning \n Model {model} | Training {training} - Test loss: {score[0]}")
+print(f"Fine-tuning after Transfer-learning \n Model {model} | Training {training} - Test accuracy: {round(score[1]*100, 2)} %")
+
